b69ba2b470d98883f00d654480a24def64c039b2
Dino Koutrouzas <dino@oat-bot.com>
2019-02-22 16:16:56 +0000

    highlight current line

--- init.el
+++ init.el
@@ -61,6 +61,8 @@
                     :width 'normal)
 (setq-default line-spacing 4)
 
+(global-hl-line-mode 1)
+
 (require 'helm)
 (require 'helm-projectile)
 (require 'helm-ag)
@@ -118,5 +120,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
 (put 'downcase-region 'disabled nil)
