55a2bd8c8d5c581d89c33767729a68dbb18ac3d1
Dino Koutrouzas <dino@oat-bot.com>
2019-02-22 16:17:14 +0000

    increase mode line height

--- init.el
+++ init.el
@@ -120,4 +120,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "#515151" :foreground "#a09f93" :box nil :height 1.2)))))
 (put 'downcase-region 'disabled nil)
