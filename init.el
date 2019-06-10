a88158367ac02a1e71a3e5f735c28f43c53a7a8f
Dino Koutrouzas <dino@oat-bot.com>
2019-06-10 15:16:19 +0100

    Remove unused packages and tidy up

--- init.el
+++ init.el
@@ -18,20 +18,18 @@
                      helm-projectile
                      helm-ag
                      ruby-electric
-                     seeing-is-believing
                      rbenv
                      inf-ruby
                      ruby-test-mode
                      rubocop
-                     rinari
                      exec-path-from-shell
-                     web-mode
                      auto-complete
                      magit
                      git-gutter+
                      git-gutter-fringe+
                      rust-mode
-                     writeroom-mode))
+                     writeroom-mode
+                     emojify))
 
 ; install the missing packages
 (dolist (package package-list)
@@ -49,7 +47,7 @@
   (exec-path-from-shell-initialize))
 
 (setq auto-save-file-name-transforms
-                `((".*" ,(concat user-emacs-directory "auto-save/") t))) 
+                `((".*" ,(concat user-emacs-directory "auto-save/") t)))
 
 (setq inhibit-splash-screen t
       initial-scratch-message nil
@@ -69,6 +67,7 @@
                     :height 150
                     :weight 'normal
                     :width 'normal)
+
 (setq-default line-spacing 4)
 
 (global-hl-line-mode 1)
@@ -76,11 +75,12 @@
 (require 'helm)
 (require 'helm-projectile)
 (require 'helm-ag)
+
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "s-f") #'helm-projectile-ag)
 (global-set-key (kbd "s-t") #'helm-projectile-find-file)
 
-;; Allow hash to be entered  
+;; Allow hash to be entered
 (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
 
 ;; Autoclose paired syntax elements like parens, quotes, etc
@@ -89,25 +89,10 @@
 (global-rbenv-mode)
 (rbenv-use-global)
 
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-
 (ac-config-default)
 (setq web-mode-ac-sources-alist
   '(("css" . (ac-source-css-property))
     ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
-
-(defun web-mode-indent ()
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)
-    (setq web-mode-indent-style 2)
-)
-(add-hook 'web-mode-hook  'web-mode-indent)
-(setq-default indent-tabs-mode nil)
-
-(setq seeing-is-believing-prefix "C-.")
-(add-hook 'ruby-mode-hook 'seeing-is-believing)
-(require 'seeing-is-believing)
 
 (add-hook 'ruby-mode-hook #'rubocop-mode)
 
@@ -117,20 +102,17 @@
 (require 'ruby-test-mode)
 (add-hook 'ruby-mode-hook 'ruby-test-mode)
 
-(require 'rinari)
-(global-rinari-mode)
 (require 'ido)
 (ido-mode t)
 
+(add-hook 'after-init-hook #'global-emojify-mode)
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
 (global-set-key (kbd "C-x g") 'magit-status)
 
-(add-hook 'compilation-finish-functions
-          (lambda (buf strg)
-            (switch-to-buffer-other-window "*compilation*")
-            (read-only-mode)
-            (goto-char (point-max))
-            (local-set-key (kbd "q")
-(lambda () (interactive) (quit-restore-window)))))
+(setq css-indent-offset 2)
+(setq js-indent-level 2)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -139,7 +121,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (writeroom-mode rust-mode magit solarized-theme seeing-is-believing ruby-test-mode ruby-electric rbenv inf-ruby helm-projectile helm-ag better-defaults))))
+    (emojify writeroom-mode rust-mode magit ruby-test-mode ruby-electric rbenv inf-ruby helm-projectile helm-ag better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
